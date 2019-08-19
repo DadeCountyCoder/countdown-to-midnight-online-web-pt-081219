1a0176721f81ad9b9ff272fa010db5eb905396b7
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-19 01:54:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,7 @@
 #write your code here
 
-def 
+def '#countdown' do
+    let(:countdown_output) { "10 SECOND(S)!\n9 SECOND(S)!\n8 SECOND(S)!\n7 SECOND(S)!\n6 SECOND(S)!\n5 SECOND(S)!\n4 SECOND(S)!\n3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\n" }
+    
   
 end
